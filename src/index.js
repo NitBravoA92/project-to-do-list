be82467d7c332d6dc5584dc79455d03a5ccb6876
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-06 17:52:45 -0400

    Create the toDoTasks array of objects

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const toDoTasks = [
+  {
+    description: 'Build the contact section on Renting car website',
+    completed: false,
+    id: 1
+  },
+  {
+    description: 'Clean the house',
+    completed: false,
+    id: 2
+  },
+  {
+    description: 'Complete the Mongodb course',
+    completed: false,
+    id: 3
+  },
+  {
+    description: 'Attend the AI ​​conference',
+    completed: false,
+    id: 4
+  },
+  {
+    description: 'Take the car to the workshop',
+    completed: false,
+    id: 5
+  },
+  {
+    description: 'Do the grocery shopping',
+    completed: false,
+    id: 6
+  }
+];
