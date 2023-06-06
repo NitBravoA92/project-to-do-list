3ca5cd8202c3c3fbf87574a7d20f546e78059124
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-06 18:01:03 -0400

    Install the webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -12,6 +15,9 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
