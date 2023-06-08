b81187658ce8bfbc053c983ecf6639c0c5d4073b
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-07 23:29:58 -0400

    Remove all task objects from the toDoTasks array

--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
 import './style.css';
 
-const toDoTasks = [
-  {
-    description: 'Build the contact section on Renting car website',
-    completed: false,
-    id: 1,
-  },
-  {
-    description: 'Clean the house',
-    completed: false,
-    id: 2,
-  },
-  {
-    description: 'Complete the Mongodb course',
-    completed: false,
-    id: 3,
-  },
-  {
-    description: 'Attend the AI ​​conference',
-    completed: false,
-    id: 4,
-  },
-  {
-    description: 'Take the car to the workshop',
-    completed: false,
-    id: 5,
-  },
-  {
-    description: 'Do the grocery shopping',
-    completed: false,
-    id: 6,
-  },
-];
+const toDoTasks = [];
 
 const markUpTask = (task) => `
     <li class="task ${task.completed ? 'completed' : ''}" data-index="${task.id}">
