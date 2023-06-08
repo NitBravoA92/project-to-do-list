6b34d19ef43a2929cea0203908b5370c08543a5d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-07 23:41:05 -0400

    Create two functions in class ToDoList to get and set the data in localStorage

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,5 +2,13 @@ class ToDoList {
   constructor() {
     this.tasksList = [];
   }
-  
+  getTasksListFromStore = () => {
+    const tasks = localStorage.getItem("tasks-list");
+    if (tasks) {
+      this.tasksList = [...JSON.parse(tasks)];
+    }
+  };
+  setTasksListToStore = () => {
+    localStorage.setItem("tasks-list", JSON.stringify(this.tasksList));
+  };
 }
