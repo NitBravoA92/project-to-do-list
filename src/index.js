6d04a6fd7633af74b26af000d7b7ab34b1ddff1e
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-07 23:50:48 -0400

    Move all tasks code to the ToDoList module

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
 import './style.css';
 
-const toDoTasks = [];
 
-const markUpTask = (task) => `
-    <li class="task ${task.completed ? 'completed' : ''}" data-index="${task.id}">
-      <div class="task-content">
-        <div class="mark-task">
-          <input type="checkbox" id="task-${task.id}-status" ${task.completed ? 'checked' : ''}>
-          <label for="task-${task.id}-status"></label>
-        </div>
-        <h3 class="task-description" contenteditable="true">${task.description}</h3>
-      </div>
-      <div class="task-options">
-        <div class="drag-icon">
-          <span class="dot"></span>
-          <span class="dot"></span>
-          <span class="dot"></span>
-        </div>
-        <button type="button" class="remove-task">X</button>
-      </div>
-    </li>
-  `;
-
-const toDoList = () => {
-  const listItems = toDoTasks.map((task) => markUpTask(task));
-  document.querySelector('#to-do-list').innerHTML = listItems.join('');
-};
-
-document.addEventListener('DOMContentLoaded', toDoList);
