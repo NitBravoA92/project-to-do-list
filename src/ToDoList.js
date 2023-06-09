5524d0f4a3706baafee1d26c9e8ab0255974c590
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-09 00:28:52 -0400

    Fix some indentation, spacing and sintax issues.

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,4 +1,6 @@
-import { isEmpty, markUpTask, saveLocalStorage, retrieveLocalStorage, cloneCollection, generateTask } from './Utils.js';
+import {
+  isEmpty, markUpTask, saveLocalStorage, retrieveLocalStorage, cloneCollection, generateTask,
+} from './Utils.js';
 
 class ToDoList {
   constructor() {
@@ -12,7 +14,7 @@ class ToDoList {
 
   renderTasksList = () => {
     const listItems = this.tasksList.map((task) => markUpTask(task));
-    document.querySelector("#to-do-list").innerHTML = listItems.join("");
+    document.querySelector('#to-do-list').innerHTML = listItems.join('');
     if (!isEmpty(this.tasksList)) {
       this.generalEventHandlers();
     }
@@ -24,9 +26,9 @@ class ToDoList {
       const id = this.tasksList.length + 1;
       const newTask = generateTask(inputDescription, false, id);
       this.tasksList.push(newTask);
-      inputDesc.value = "";
+      inputDesc.value = '';
       inputDesc.focus();
-      saveLocalStorage("tasks-list", JSON.stringify(this.tasksList));
+      saveLocalStorage('tasks-list', JSON.stringify(this.tasksList));
       this.renderTasksList();
     }
   };
@@ -45,8 +47,8 @@ class ToDoList {
     const taskId = taskItem.dataset.index;
     const index = Number(taskId) - 1;
     this.tasksList[index].completed = taskCompleted;
-    saveLocalStorage("tasks-list", JSON.stringify(this.tasksList));
-    taskItem.classList.toggle("completed");
+    saveLocalStorage('tasks-list', JSON.stringify(this.tasksList));
+    taskItem.classList.toggle('completed');
   };
 
   deleteTask = (event) => {
@@ -55,11 +57,11 @@ class ToDoList {
     const taskId = Number(taskItem.dataset.index);
     if (!isEmpty(this.tasksList)) {
       const newTaskList = this.tasksList.filter(
-        (task) => task.index !== taskId
+        (task) => task.index !== taskId,
       );
       this.tasksList = cloneCollection(newTaskList);
       this.setTasksIds();
-      saveLocalStorage("tasks-list", JSON.stringify(this.tasksList));
+      saveLocalStorage('tasks-list', JSON.stringify(this.tasksList));
       this.renderTasksList();
     }
   };
@@ -69,80 +71,80 @@ class ToDoList {
       const newTasksList = this.tasksList.filter((task) => !task.completed);
       this.tasksList = cloneCollection(newTasksList);
       this.setTasksIds();
-      saveLocalStorage("tasks-list", JSON.stringify(this.tasksList));
+      saveLocalStorage('tasks-list', JSON.stringify(this.tasksList));
       this.renderTasksList();
     }
   };
 
   onTaskFocus = (event) => {
     const taskItem = event.target.parentElement.parentElement;
-    const btnRemove = taskItem.querySelector(".remove-task");
-    taskItem.classList.add("focus");
-    btnRemove.style.display = "block";
-    btnRemove.style.opacity = "1";
-    btnRemove.removeAttribute("disabled");
+    const btnRemove = taskItem.querySelector('.remove-task');
+    taskItem.classList.add('focus');
+    btnRemove.style.display = 'block';
+    btnRemove.style.opacity = '1';
+    btnRemove.removeAttribute('disabled');
   };
 
   onTaskFocusOut = (event) => {
     const taskItem = event.target.parentElement.parentElement;
-    const btnRemove = taskItem.querySelector(".remove-task");
-    taskItem.classList.remove("focus");
-    btnRemove.style.opacity = "0";
+    const btnRemove = taskItem.querySelector('.remove-task');
+    taskItem.classList.remove('focus');
+    btnRemove.style.opacity = '0';
     setTimeout(() => {
-      btnRemove.setAttribute("disabled", "true");
-      btnRemove.style.display = "none";
+      btnRemove.setAttribute('disabled', 'true');
+      btnRemove.style.display = 'none';
     }, 500);
-    saveLocalStorage("tasks-list", JSON.stringify(this.tasksList));
+    saveLocalStorage('tasks-list', JSON.stringify(this.tasksList));
   };
 
   createTaskEventHandler = () => {
-    const inputDescription = document.querySelector("#inputTaskDescription");
-    const btnAddTask = document.querySelector("#btnAddTask");
-    inputDescription.addEventListener("keydown", (event) => {
-      if (event.code === "Enter") {
+    const inputDescription = document.querySelector('#inputTaskDescription');
+    const btnAddTask = document.querySelector('#btnAddTask');
+    inputDescription.addEventListener('keydown', (event) => {
+      if (event.code === 'Enter') {
         this.createTask(inputDescription);
       }
     });
-    btnAddTask.addEventListener("click", () => {
+    btnAddTask.addEventListener('click', () => {
       this.createTask(inputDescription);
     });
   };
 
   generalEventHandlers() {
-    const allTaskDescription = document.querySelectorAll(".task-description");
-    const allTaskBtnRemove = document.querySelectorAll(".remove-task");
-    const allTaskCheckbox = document.querySelectorAll(".task-check-status");
+    const allTaskDescription = document.querySelectorAll('.task-description');
+    const allTaskBtnRemove = document.querySelectorAll('.remove-task');
+    const allTaskCheckbox = document.querySelectorAll('.task-check-status');
     allTaskDescription.forEach((task) => {
-      task.addEventListener("keyup", this.updateTask);
-      task.addEventListener("focusin", this.onTaskFocus);
-      task.addEventListener("focusout", this.onTaskFocusOut);
+      task.addEventListener('keyup', this.updateTask);
+      task.addEventListener('focusin', this.onTaskFocus);
+      task.addEventListener('focusout', this.onTaskFocusOut);
     });
     allTaskBtnRemove.forEach((btnRemove) => {
-      btnRemove.addEventListener("click", this.deleteTask);
+      btnRemove.addEventListener('click', this.deleteTask);
     });
     allTaskCheckbox.forEach((inputStatus) => {
-      inputStatus.addEventListener("change", this.UpdateTaskStatus);
+      inputStatus.addEventListener('change', this.UpdateTaskStatus);
     });
   }
 
   clearAllEventHandler = () => {
     document
-      .querySelector("#btn-clear-list")
-      .addEventListener("click", this.deleteAllCompletedTasks);
+      .querySelector('#btn-clear-list')
+      .addEventListener('click', this.deleteAllCompletedTasks);
   };
 
   listTasks = () => {
-    this.tasksList = cloneCollection(JSON.parse(retrieveLocalStorage("tasks-list")));
+    this.tasksList = cloneCollection(JSON.parse(retrieveLocalStorage('tasks-list')));
     this.renderTasksList();
     this.createTaskEventHandler();
     this.clearAllEventHandler();
   };
 
   refreshListEventHandler = () => {
-    document.querySelector("#refresh").addEventListener("click", (event) => {
+    document.querySelector('#refresh').addEventListener('click', (event) => {
       const rotateButton = [
-        { transform: "rotate(-90deg)" },
-        { transform: "rotate(90deg)" },
+        { transform: 'rotate(-90deg)' },
+        { transform: 'rotate(90deg)' },
       ];
       event.target.animate(rotateButton, { duration: 300, iterations: 1 });
       this.listTasks();
