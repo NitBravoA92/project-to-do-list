dedad472226d40ce407a0357ddb690db40cc9a98
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-14 17:28:30 -0400

    Update createTask function

--- src/modules/createTask.js
+++ src/modules/createTask.js
@@ -1,30 +1,31 @@
 import {
-  retrieveLocalStorage, saveLocalStorage, taskObj, isEmpty,
+  retrieveLocalStorage, saveLocalStorage, taskObj, isEmpty, markUpTask
 } from './Utils.js';
 import {
-  insertTaskInDom,
   loadTasksEventHandlers,
 } from './renderTask.js';
 
-export const createTask = (inputDesc) => {
-  const inputTaskDescription = inputDesc.value;
+export const createTask = (inputTaskDescription) => {
   const tasksList = retrieveLocalStorage('tasks-list');
-  if (!isEmpty(inputTaskDescription)) {
     const id = tasksList.length + 1;
     const newTask = taskObj(inputTaskDescription, false, id);
     tasksList.push(newTask);
-    inputDesc.value = '';
-    inputDesc.focus();
     saveLocalStorage('tasks-list', JSON.stringify(tasksList));
-    insertTaskInDom(newTask);
-    loadTasksEventHandlers();
-  }
+    const newItem = markUpTask(newTask);
+    return newItem;
 };
 
 export const createTaskEventHandler = () => {
   const taskForm = document.querySelector('#new-task-entry');
   taskForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    createTask(taskForm.inputTaskDescription);
+    const inputDesc = taskForm.inputTaskDescription; 
+    if (!isEmpty(inputDesc.value)) {
+      const newItem = createTask(inputDesc.value);
+      document.querySelector('#to-do-list').innerHTML += newItem;
+      inputDesc.value = '';
+      inputDesc.focus();
+      loadTasksEventHandlers();
+    }
   });
 };
