13a51b7eb5910f0740171b59d18b58212e9c5d36
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-14 17:30:45 -0400

    Create two test cases to validate createTask function

--- src/__tests__/createTask.test.js
+++ src/__tests__/createTask.test.js
@@ -1,12 +1,28 @@
 import {describe, expect} from '@jest/globals';
+import { createTask } from '../modules/createTask.js';
+import { saveLocalStorage, retrieveLocalStorage } from '../modules/Utils.js';
 
 describe('Create new tasks module', () => {
-  it('insert a task, to have 2 items in the container', () => {
-    document.body.innerHTML =
-      '<div>' +
-      '  <ul id="list"><li></li><li></li></ul>' +
-      '</div>';
-    const list = document.querySelectorAll('#list li');
-    expect(list).toHaveLength(2);
+  it('insert a task, to have 1 li in the container', () => {
+    const descriptionTest = 'First task description';
+    const liElement = createTask(descriptionTest);
+
+    document.body.innerHTML = `
+      <ul id="to-do-list">${liElement}</ul>
+    `;
+
+    const listTasks = document.querySelectorAll('#to-do-list li');
+    expect(listTasks).toHaveLength(1);
+  })
+  it('insert 3 tasks, to have 4 li items in the container', () => {
+    const descriptionTest = ['First task description', 'Second task description', 'Third task description'];
+    const liElements = descriptionTest.map((desc) => createTask(desc)).join('');
+    
+    document.body.innerHTML = `
+      <ul id="to-do-list"><li></li>${liElements}</ul>
+    `;
+
+    const listTasks = document.querySelectorAll('#to-do-list li');
+    expect(listTasks).toHaveLength(4);
   })
 })
