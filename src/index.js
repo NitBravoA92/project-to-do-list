740a9a503e6f8326e5e5dc29fc9bb51bc285c43a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-14 00:09:21 -0400

    import and render images dinamically from javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { listTasks, refreshListEventHandler } from './modules/listTask.js';
+import { insertImgMarkup } from './modules/Utils.js';
+import enterIcon from './img/enterIcon.svg';
+import refreshIcon from './img/refreshIcon.svg';
 import './style.css';
 
 const ToDoList = () => {
+  const addBtn = document.querySelector('#btnAddTask');
+  const refreshBtn = document.querySelector('#refresh');
+  insertImgMarkup(addBtn, enterIcon, 'left arrow icon');
+  insertImgMarkup(refreshBtn, refreshIcon, 'refresh icon');
   listTasks();
   refreshListEventHandler();
 };
--- src/modules/Utils.js
+++ src/modules/Utils.js
@@ -1,3 +1,5 @@
+import trashIcon from '../img/trashIcon.svg';
+
 export const retrieveLocalStorage = (key) => {
   const data = localStorage.getItem(key)
               ? JSON.parse(localStorage.getItem(key))
@@ -31,9 +33,12 @@ export const markUpTask = (task) => `
     </div>
   </div>
   <button type="button" class="remove-task" disabled>
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#bebebe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+    <img src="${trashIcon}" alt="gray trash icon" />
   </button>
 </li>
 `;
+export const insertImgMarkup = (container, icon, alt) => {
+  container.innerHTML = `<img src="${icon}" alt="${alt}">`;
+};
 export const cloneCollection = (collection) => [...collection];
 export const taskObj = (description, completed, index) => ({ description, completed, index });
