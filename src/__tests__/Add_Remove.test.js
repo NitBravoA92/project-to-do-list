3ef6221c43fa2d9baf5d093d95e8dc15e54da226
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-14 18:32:34 -0600

    Create test case to remove task functionality

--- src/__tests__/Add_Remove.test.js
+++ src/__tests__/Add_Remove.test.js
@@ -1,6 +1,6 @@
-import {describe, expect} from '@jest/globals';
 import { createTask } from '../modules/createTask.js';
 import { saveLocalStorage, retrieveLocalStorage } from '../modules/Utils.js';
+import { deleteTask } from '../modules/deleteTask.js';
 
 describe('Create new tasks module', () => {
   it('insert a task, to have 1 li in the container', () => {
@@ -13,19 +13,20 @@ describe('Create new tasks module', () => {
 
     const listTasks = document.querySelectorAll('#to-do-list li');
     expect(listTasks).toHaveLength(1);
-  })
+  });
+
   it('insert 3 tasks, to have 4 li items in the container', () => {
     const descriptionTest = ['First task description', 'Second task description', 'Third task description'];
     const liElements = descriptionTest.map((desc) => createTask(desc)).join('');
-    
+
     document.body.innerHTML = `
       <ul id="to-do-list"><li></li>${liElements}</ul>
     `;
 
     const listTasks = document.querySelectorAll('#to-do-list li');
     expect(listTasks).toHaveLength(4);
-  })
-})
+  });
+});
 
 describe('Insert in localStorage', () => {
   it('save one task object', () => {
@@ -33,7 +34,26 @@ describe('Insert in localStorage', () => {
     const mockKey = 'task-list';
 
     saveLocalStorage(mockKey, JSON.stringify(mockObj));
-    
+
     expect(retrieveLocalStorage(mockKey)).toEqual(mockObj);
-  })
-})
+  });
+});
+
+describe('Remove task module', () => {
+  it('Delete second task from group of 3', () => {
+    document.body.innerHTML = `
+      <ul id="to-do-list-remove">
+        <li class="task" data-index="1">Hello</li>
+        <li class="task" data-index="2">Hello 2</li>
+        <li class="task" data-index="3">Hello 3</li>
+      </ul>
+    `;
+    const liElements = document.querySelectorAll('.task');
+
+    deleteTask(liElements[1]);
+
+    const newLiElements = document.querySelectorAll('#to-do-list-remove .task');
+
+    expect(newLiElements).toHaveLength(2);
+  });
+});
